f44d3b72ca1be38089423c5cacf07036f96ff20d
Maks Senuk <72966200+makssenuk2002@users.noreply.github.com>
2021-11-15 08:52:05 +0300

    last

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 import data from './data.js';
-
 data.columns.forEach((el, id) => {
-    const cont = document.getElementById('cont');
-    const cont_block = document.createElement('div');
+    const content = document.getElementById('content');
+    const content_block = document.createElement('div');
     const body = document.createElement('div');
     const header = document.createElement('div');
     const body_text = data.rows[0].values[id];
     header.innerHTML = el.name;
 
     if (el.dataType === 'DateTime') {
-        let date = new Date(data.rows[0].values[id]).toLocaleDateString("ru-RU");
-        body.innerHTML = date;
+        body.innerHTML = new Date(data.rows[0].values[id]).toLocaleDateString("ru-RU");
     } else {
         body.innerHTML = body_text;
     }
 
-    cont_block.appendChild(header);
-    cont_block.appendChild(body);
-    cont.appendChild(cont_block);
+    content_block.appendChild(header);
+    content_block.appendChild(body);
+    content.appendChild(content_block);
 
-    cont_block.classList.add('content__block');
+    content_block.classList.add('content__block');
     header.classList.add('block__header');
     body.classList.add('block__body');
 })
